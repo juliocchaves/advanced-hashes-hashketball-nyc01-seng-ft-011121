349990444a185d35fd53f9d054df5ce50cca4ffc
juliochavesnyc <juliochavesnyc@gmail.com>
2020-12-08 21:14:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -132,7 +132,6 @@ end
 def num_points_scored(players)
 game_hash.each do |team, team_info|
     team_info[:players].each do |player|
-        binding.pry
       if player[:player_name] == player_search
         return player[:points]
       end
