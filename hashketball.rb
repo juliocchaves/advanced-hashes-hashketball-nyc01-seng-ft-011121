e028c6dd5f2f7cb3b92e8b4f2a7edc4b0c406ee9
juliochavesnyc <juliochavesnyc@gmail.com>
2020-12-08 21:20:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -130,12 +130,11 @@ end
 # Write code here
 
 def num_points_scored(player)
-game_hash.each do |team, team_info|
-    team_info[:players].each do |player|
-      if player[:player_name] == player_search
+ game_hash.each do |home_away, keys|
+    keys[:players].each do |player|
+      return player[:points] if player[:player_name] == player_n
     end
   end
-end
 end
 
 def shoe_size(name)
